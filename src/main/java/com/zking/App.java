d87590f5a8155376d612358f7f4cb74e4358f031
xiexi997 <2296356131@qq.com>
2019-12-14 14:48:23 +0800

    提交说明

--- src/main/java/com/zking/App.java
+++ src/main/java/com/zking/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args ){
         int a = 10;
         int b = 5;
+        int c = a+b;
         System.out.println( "Hello World!" );
     }
 }
