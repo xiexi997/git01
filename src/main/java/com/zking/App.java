b99192eee94b7660b74ec5c3f6038b5b9e3e02ec
xiexi997 <2296356131@qq.com>
2019-12-14 14:25:21 +0800

    这是T232班提交的代码

--- src/main/java/com/zking/App.java
+++ src/main/java/com/zking/App.java
@@ -6,8 +6,9 @@ package com.zking;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ){
+        int a = 10;
+        int b = 5;
         System.out.println( "Hello World!" );
     }
 }
